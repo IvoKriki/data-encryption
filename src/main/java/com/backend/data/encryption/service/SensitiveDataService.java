53f542eb22dd83fa99822e0881c957d72902865d
Ivo Kriki <ivo.kriki@outlook.com>
2024-07-29 23:10:46 +0100

    created service methods

--- src/main/java/com/backend/data/encryption/service/SensitiveDataService.java
+++ src/main/java/com/backend/data/encryption/service/SensitiveDataService.java
@@ -1,7 +1,27 @@
 package com.backend.data.encryption.service;
 
+import com.backend.data.encryption.entity.SensitiveData;
+import com.backend.data.encryption.repository.SensitiveDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SensitiveDataService {
+
+    @Autowired
+    private SensitiveDataRepository sensitiveDataRepository;
+
+    public List<SensitiveData> findAll(){
+        return sensitiveDataRepository.findAll();
+    }
+
+    public SensitiveData save(SensitiveData data){
+        return sensitiveDataRepository.save(data);
+    }
+
+    public void deleteById(Long id){
+        sensitiveDataRepository.deleteById(id);
+    }
 }
